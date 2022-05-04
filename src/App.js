522a8579bbe15f769d7b4a8c42693784218f889f
H0RSESH0E <94808313+H0RSESH0E@users.noreply.github.com>
2022-05-04 17:35:28 -0400

    Hooks added: document.title changing with useEffect

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery'
 
-function App() {
 
+function App() {
+  const [categories] = useState([
+    {
+      name: 'commercial',
+      description: 'Photos of grocery stores, food trucks, and other commercial projects',
+    },
+    { name: 'portraits', description: 'Portraits of people in my life' },
+    { name: 'food', description: 'Delicious delicacies' },
+    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[1]);
   return (
     <div>
-      <Nav />
+      <Nav categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory} />
       <main>
         <Gallery />
-          <About />        
+        <About />
       </main>
     </div>
   );
